c07970b67215e7f3a57fe4c79500042da35c28ad
Zach Dwyer <zdwyer@gmail.com>
2023-08-10 22:02:40 -0400

    remove prints

--- R/transcriptomicTab.R
+++ R/transcriptomicTab.R
@@ -37,9 +37,7 @@ transcriptomicTabServer <- function(id) {
 		})
 		
 		observeEvent(gene_options(), {
-		  print("C1")
 		  updateSelectizeInput(session, 'value', "Gene:", choices=gene_options(), selected='INS', server=TRUE)
-		  print("C2")
 		})
 		
 		seurat_obj = reactive({
@@ -55,53 +53,39 @@ transcriptomicTabServer <- function(id) {
 		})
 		
 		cluster_annotations = reactive({
-		  print("A")
 			levels(Idents(seurat_obj()))
 		})
 		
 		selected_cluster_gene = reactive({
-		  print("J1")
 		  req(cluster_DEG())
 		  req(input$cluster_DEG_rows_selected)
-		  print("J2")
 		  (cluster_DEG() %>% slice(input$cluster_DEG_rows_selected))$Gene
 		})
 		
 		observeEvent(selected_cluster_gene(), {
-		  print("I1")
 			updateSelectizeInput(session, 'value', "Gene:", choices=gene_options(), selected=selected_cluster_gene(), server=TRUE)
-			print("I2")
 		})
 		
 		observeEvent(cluster_annotations(), {
-		  print('B1')
 		  req(cluster_annotations())
-		  print("B2")
-		  print(cluster_annotations())
 			updateSelectizeInput(session, 'cluster', "Cluster:", choices=cluster_annotations(), selected=cluster_annotations()[1], server=TRUE)
 		})
 		
 		cluster_DEG = reactive({
-		  print('D1')
 		  req(input$cluster)
-		  print("D2")
 			read.delim(paste0('data/raw/DEG/organic/', input$cluster, '_2vs11_organic.csv'), sep=',', col.names = c("Gene", "p", "avg_log2FC", "pct.1", "pct.2", "adjusted_p"))
 		})
 		
 		output$umap_type = renderPlot({
-		  print("E1")
 			DimPlot(seurat_obj(), reduction = "umap")
 		})
 		
 		output$umap_gene = renderPlot({
-		  print("F1")
 		  req(input$value)
-		  print("F2")
 			FeaturePlot(seurat_obj(), features = c(input$value))
 		})
 		
 		output$cluster_DEG = renderDT({
-		  print("G1")
 			datatable(cluster_DEG(), 
 								selection='single',
 								options=list(
@@ -116,9 +100,7 @@ transcriptomicTabServer <- function(id) {
 		})
 		
 		output$violin_gene = renderPlot({
-		  print("H1")
 		  req(input$value)
-		  print("H2")
 			VlnPlot(seurat_obj(), features = c(input$value), split.by='Type', slot='counts', log=TRUE)
 		})
 	})

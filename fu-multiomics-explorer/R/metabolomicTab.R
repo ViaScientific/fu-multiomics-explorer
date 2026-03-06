469457fd040fb1072050c333c6e6abaa16b8a082
Zach Dwyer <zach@viascientific.com>
2026-03-06 10:14:04 -0500

    convert metabolomic to bslib and card

--- fu-multiomics-explorer/R/metabolomicTab.R
+++ fu-multiomics-explorer/R/metabolomicTab.R
@@ -1,23 +1,20 @@
 metabolomicTabUI <- function(id) {
 	ns <- NS(id)
 	tagList(
-		box(width=6, title="Metabolite Quantification", status='primary', solidHeader = TRUE,
-				selectizeInput(ns("value"), "Selection:", choices=NULL),
-				barplotUI(ns('barplot'))
-		),
-		box(width=6, title="Metabolite Comparison", status='primary', solidHeader = TRUE,
-				fluidRow(
-					column(6, selectizeInput(ns("x"), "X:", choices=NULL)),
-					column(6, selectizeInput(ns("y"), "Y:", choices=NULL))
-				),
-				conditionalPanel("input.x != '' & input.y != ''",
-												 scatterplotUI(ns('comparison')),
-												 ns=ns
-				)
-		),
-		box(width=6, title="Metadata", status='primary', solidHeader = TRUE,
-				DTOutput(ns("metadata")) %>% withSpinner(image='spinner.gif')
-		)
+	  barplotUI(ns('barplot'))
+		#box(width=6, title="Metabolite Comparison", status='primary', solidHeader = TRUE,
+		#		fluidRow(
+		#			column(6, selectizeInput(ns("x"), "X:", choices=NULL)),
+		#			column(6, selectizeInput(ns("y"), "Y:", choices=NULL))
+		#		),
+		#		conditionalPanel("input.x != '' & input.y != ''",
+		#										 scatterplotUI(ns('comparison')),
+		#										 ns=ns
+		#		)
+		#),
+		#box(width=6, title="Metadata", status='primary', solidHeader = TRUE,
+		#		DTOutput(ns("metadata")) %>% withSpinner(image='spinner.gif')
+		#)
 	)
 }
 
@@ -30,7 +27,7 @@ metabolomicTabServer <- function(id) {
 				mutate(Donor=as.factor(Donor))
 		})
 		
-		data = reactive({
+		raw_data = reactive({
 			read.delim(file=clean_data_path('B1.txt'), header=TRUE) %>%
 				pivot_longer(cols=starts_with('D')) %>%
 				mutate(Donor=substr(name, 2,2)) %>%
@@ -40,60 +37,34 @@ metabolomicTabServer <- function(id) {
 				left_join(metadata(), by='Donor')
 		})
 				
-		options = reactive({
-			(data() %>% arrange(ionTopName) %>% distinct(ionTopName))$ionTopName
-		})
+		barplotServer('barplot', raw_data, 'ionTopName', 'Metabolite', 'Type', 'None')
 		
-		observeEvent(data(), {
-			updateSelectizeInput(session, 'value', "Selection:", choices=options(), selected='', server=TRUE)
-			updateSelectizeInput(session, 'x', "X:", choices=options(), selected='', server=TRUE)
-			updateSelectizeInput(session, 'y', "Y:", choices=options(), selected='', server=TRUE)
-		})
-
-		filtered_data = reactive({
-			req(input$value)
-			data() %>% filter(ionTopName == input$value)
-		})
-
-		output$out = renderPlot({
-			req(input$value)
-			ggplot(filtered_data(), aes(x=Donor, y=Value, fill=Type)) +
-				theme_classic() +
-				theme(plot.title = element_text(hjust = 0.5)) +
-				ggtitle(input$value) +
-				scale_y_continuous(expand=c(0,0)) +
-				geom_bar(stat='identity', position=position_dodge(.9))
-		})
-		
-		output$metadata = renderDT({
-			datatable(metadata(),
-								selection='single',
-								rownames=FALSE,
-								colnames=c("Donor", "ID", "Source", "Age", "BMI", "Gender", "ComparisonID", "ComparisonID2"),
-								options=list(
-								  dom='t',
-								  columnDefs = list(list(visible=FALSE, targets=c(6,7)))          
-								)
-			)
-		})
-		
-		comparison_data = reactive({
-			req(input$x)
-			req(input$y)
-			x = data() %>% filter(ionTopName == input$x) %>% rename(!!input$x := Value) %>% select(-ionTopName)
-			y = data() %>% filter(ionTopName == input$y) %>% rename(!!input$y := Value) %>% select(-ionTopName)
-			combined = x %>% left_join(y, by=c("Donor", "Type", "ID", "Source", "Age", "BMI", "Gender"))
-		})
-		
-		download_data = reactive({
-		  req(input$x)
-		  req(input$y)
-		  data() %>% filter(ionTopName == input$x | ionTopName == input$y) %>% select(Donor, Type, ionTopName, Value)
-		})
-		
-		barplotServer('barplot', filtered_data, 'Type', 'None', filtered_data)
-		
-		scatterplotServer('comparison', comparison_data, reactive(input$x), reactive(input$y), download_data)
+		#output$metadata = renderDT({
+		#	datatable(metadata(),
+		#						selection='single',
+		#						rownames=FALSE,
+		#						colnames=c("Donor", "ID", "Source", "Age", "BMI", "Gender", "ComparisonID", "ComparisonID2"),
+		#						options=list(
+		#						  dom='t',
+		#						  columnDefs = list(list(visible=FALSE, targets=c(6,7)))          
+		#						)
+		#	)
+		#})
+		#
+		#observeEvent(raw_data(), {
+		#  updateSelectizeInput(session, 'x', "X:", choices=options(), selected='', server=TRUE)
+		#  updateSelectizeInput(session, 'y', "Y:", choices=options(), selected='', server=TRUE)
+		#})
+		#
+		#comparison_data = reactive({
+		#	req(input$x)
+		#	req(input$y)
+		#	x = raw_data() %>% filter(ionTopName == input$x) %>% rename(!!input$x := Value) %>% select(-ionTopName)
+		#	y = raw_data() %>% filter(ionTopName == input$y) %>% rename(!!input$y := Value) %>% select(-ionTopName)
+		#	combined = x %>% left_join(y, by=c("Donor", "Type", "ID", "Source", "Age", "BMI", "Gender"))
+		#})
+		#
+		#scatterplotServer('comparison', comparison_data, reactive(input$x), reactive(input$y), download_data)
 		
 		return(data)
 	})
